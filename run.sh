92fe814314b23354cb61ee7aceccf373a7b464bc
root <root@localhost.localdomain>
2022-08-01 13:43:58 +0800

    change Dockerfile

--- run.sh
+++ run.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+BLOCKBOOK_PATH="${BLOCKBOOK_PATH:-/root/blockbook}"
+
 RPC_USER="${RPC_USER:-chain}"
 RPC_PASS="${RPC_PASS:-999000}"
-RPC_HOST="${RPC_HOST:-localhost}"
-RPC_PORT="${RPC_PORT:-9280}"
-MQ_PORT="${MQ_URL:-38393}"
+RPC_HTTP="${RPC_HTTP:-http}"
+RPC_HOST="${RPC_HOST:-127.0.0.1}"
+RPC_PORT="${RPC_PORT:-9282}"
+MQ_PORT="${MQ_PORT:-38393}"
 
-CFG_FILE=/home/blockbook/build/blockchaincfg.json
+NEVM_HTTP="${NEVM_HTTP:-http}"
+NEVM_HOST="${NEVM_HOST:-localhost}"
+NEVM_PORT="${NEVM_PORT:-4000}"
+
+CFG_FILE=$BLOCKBOOK_PATH/build/blockchaincfg.json
 
 sed -i 's/\"rpc_user\":.*/\"rpc_user\": \"'${RPC_USER}'\",/g' $CFG_FILE
 sed -i 's/\"rpc_pass\":.*/\"rpc_pass\": \"'${RPC_PASS}'\",/g' $CFG_FILE
-sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"http:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"'${RPC_HTTP}':\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
 sed -i 's/\"message_queue_binding\":.*/\"message_queue_binding\": \"tcp:\/\/'${RPC_HOST}':'${MQ_PORT}'\",/g' $CFG_FILE
 
+HTML_FILE=$BLOCKBOOK_PATH/static/templates/asset.html
+sed -i 's/http:\/\/127.0.0.1:4000/'${NEVM_HTTP}':\/\/'${NEVM_HOST}':'${NEVM_PORT}'/g' $HTML_FILE
 
 exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9130} -logtostderr
 
--- start.sh
+++ start.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+BLOCKBOOK_PATH="${BLOCKBOOK_PATH:-/root/blockbook}"
+RPC_USER="${RPC_USER:-chain}"
+RPC_PASS="${RPC_PASS:-999000}"
+RPC_HTTP="${RPC_HTTP:-http}"
+RPC_HOST="${RPC_HOST:-127.0.0.1}"
+RPC_PORT="${RPC_PORT:-9282}"
+MQ_PORT="${MQ_PORT:-38393}"
 
-nohup ./blockbook -sync -blockchaincfg=build/blockchaincfg.json -internal=:9030 -public=:9130 -logtostderr &
+NEVM_HTTP="${NEVM_HTTP:-http}"
+NEVM_HOST="${NEVM_HOST:-localhost}"
+NEVM_PORT="${NEVM_PORT:-4000}"
+
+CFG_FILE=$BLOCKBOOK_PATH/build/blockchaincfg.json
+
+sed -i 's/\"rpc_user\":.*/\"rpc_user\": \"'${RPC_USER}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_pass\":.*/\"rpc_pass\": \"'${RPC_PASS}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"'${RPC_HTTP}':\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+sed -i 's/\"message_queue_binding\":.*/\"message_queue_binding\": \"tcp:\/\/'${RPC_HOST}':'${MQ_PORT}'\",/g' $CFG_FILE
+
+HTML_FILE=$BLOCKBOOK_PATH/static/templates/asset.html
+sed -i 's/http:\/\/127.0.0.1:4000/'${NEVM_HTTP}':\/\/'${NEVM_HOST}':'${NEVM_PORT}'/g' $HTML_FILE
+
+#nohup ./blockbook -sync -blockchaincfg=build/blockchaincfg.json -internal=:9030 -public=:9130 -logtostderr &
 
 
